26836d709e3e2ff1faa6f3c895b27539cb62ae38
antoineegret <a.egret@laposte.net>
2019-12-10 12:07:57 +0100

    exo6 partie4 php

---
+++ index.php
@@ -0,0 +1,11 @@
+
+<?php
+  $firstName = 'Antoine';
+  $lastName = 'Egret';
+  $year = 27;
+  function concat($firstName, $lastName, $year)
+  {
+    echo "Bonjour $lastName $firstName, vous avez $year ans.";
+  }
+  concat($firstName, $lastName, $year);
+ ?>
